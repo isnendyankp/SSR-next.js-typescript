47c8d7368dca4f17ab5b0628a465db0d0f5b33c1
isnendyankp <isnendyankp@gmail.com>
2024-01-20 06:12:25 +0700

    Cr8 home page as a register page

---
+++ src/components/index.ts
@@ -0,0 +1,6 @@
+//  Create file distributor
+export { default as Input } from './Input';
+export { default as Text } from './Text';
+export { default as Button } from './Button';
+export { default as Card } from './Card';
+
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,118 +1,113 @@
-import Image from "next/image";
-import { Inter } from "next/font/google";
+import { Input, Text, Button, Card } from '../components';
+import { useFormik } from 'formik';
+import * as yup from 'yup';
+import { useRouter } from 'next/router';
 
-const inter = Inter({ subsets: ["latin"] });
+const Home = () => {
+  // use router hook
+  const router = useRouter();
 
-export default function Home() {
+  // interface for form props
+  interface FormProps {
+    email: string;
+    name: string;
+    password: string;
+  }
+
+  // useFormik hook
+  const formMik = useFormik<FormProps>({
+    initialValues: {
+      email: '',
+      name: '',
+      password: '',
+    },
+
+    // onSubmit function for submit form
+    onSubmit: async (values) => {
+      await fetch('https://mock-api.arikmpt.com/api/user/register', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(values),
+      });
+      console.log('success');
+      // navigate to login page
+      router.push('/Login');
+    },
+
+    // validation for username, email & password
+    validationSchema: yup.object({
+      name: yup.string().required('name tidak boleh kosong'),
+      email: yup
+        .string()
+        .email('Email tidak valid')
+        .required('Email tidak boleh kosong'),
+      password: yup
+        .string()
+        .min(8, 'Password minimal 8 karakter')
+        .required('Password tidak boleh kosong'),
+    }),
+  });
+
   return (
-    <main
-      className={`flex min-h-screen flex-col items-center justify-between p-24 ${inter.className}`}
-    >
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/pages/index.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
+    <Card border={false} className=" ">
+      <Card border>
+        {/* form element */}
+        <form onSubmit={formMik.handleSubmit}>
+          {/* Input name */}
+          <div className="m-1">
+            <Text>{'name'}</Text>
+            <Input
+              className="block border-emerald-700 border"
+              name={'name'}
+              value={formMik.values.name}
+              onChange={formMik.handleChange('name')}
             />
-          </a>
-        </div>
-      </div>
+            {/* add error validation text with yup for input name */}
+            {formMik.errors.name && <Text>{formMik.errors.name}</Text>}
+          </div>
 
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-full sm:before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full sm:after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700/10 after:dark:from-sky-900 after:dark:via-[#0141ff]/40 before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
+          {/* Input Email */}
+          <div className="m-1">
+            <Text>{'Email'}</Text>
+            <Input
+              className="block border-emerald-700 border"
+              name={'email'}
+              value={formMik.values.email}
+              onChange={formMik.handleChange('email')}
+            />
+            {/* add error validation text with yup for input email */}
+            {formMik.errors.email && <Text>{formMik.errors.email}</Text>}
+          </div>
 
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
+          {/* Input Password */}
+          <div className="m-1">
+            <Text>{'Password'}</Text>
+            <Input
+              className="block border-emerald-700 border"
+              name={'password'}
+              type={'password'}
+              value={formMik.values.password}
+              onChange={formMik.handleChange('password')}
+            />
+            {/* add error validation text with yup for input password */}
+            {formMik.errors.password && <Text>{formMik.errors.password}</Text>}
+          </div>
 
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
+          {/* Button Submit */}
+          <Button label={'Submit'} type={'submit'} className={'bg-green-500'} />
 
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Discover and deploy boilerplate example Next.js&nbsp;projects.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50 text-balance`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
+          {/* "Login" button */}
+          <Button
+            label={'Login'}
+            onClick={() => router.push('/Login')}
+            className={'bg-blue-500 mt-2'}
+          />
+        </form>
+      </Card>
+    </Card>
   );
-}
+};
+
+export default Home;
