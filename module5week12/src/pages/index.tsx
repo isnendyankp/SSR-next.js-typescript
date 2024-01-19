c307b2b6763b6833b7e0c14b281ed9bc47aa4697
isnendyankp <isnendyankp@gmail.com>
2024-01-19 15:40:21 +0700

    Add useFormik hook, onSubmit function for register & validationSchema

--- module5week12/src/pages/index.tsx
+++ module5week12/src/pages/index.tsx
@@ -1,16 +1,56 @@
 
-// interface for form props
+import React from 'react';
+import { useFormik } from 'formik';
+import * as yup from 'yup';
+
+const Home = () => {
+  // useNavigate hook
+  // const navigate = useNavigate();
+
+  // interface for form props
   interface FormProps {
     email: string;
     name: string;
     password: string;
   }
 
+  // useFormik hook
+  const formMik = useFormik<FormProps>({
+    initialValues: {
+      email: '',
+      name: '',
+      password: '',
+    },
 
-const Home = () => {
-  return (
-    <p>ini halaman Home</p>
-  );
+    // onSubmit function for submit form
+    onSubmit: async (values) => {
+      await fetch('https://mock-api.arikmpt.com/api/user/register', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(values),
+      });
+      console.log('success');
+      // navigate to login page
+      // navigate('/login');
+    },
+
+    // validation for username, email & password
+    validationSchema: yup.object({
+      name: yup.string().required('name tidak boleh kosong'),
+      email: yup
+        .string()
+        .email('Email tidak valid')
+        .required('Email tidak boleh kosong'),
+      password: yup
+        .string()
+        .min(8, 'Password minimal 8 karakter')
+        .required('Password tidak boleh kosong'),
+    }),
+  });
+
+  return <p>ini halaman Home</p>;
 }
 
 export default Home;
