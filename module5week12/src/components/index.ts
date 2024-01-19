3a19ecdcdce0a9247e3c33b162c32928c817a70b
isnendyankp <isnendyankp@gmail.com>
2024-01-20 00:39:14 +0700

    fix error

--- module5week12/src/components/index.ts
+++
@@ -1,8 +0,0 @@
-//  Create file distributor
-
-export { default as Input } from './Input';
-export { default as Text } from './Text';
-export { default as Button } from './Button';
-export { default as Card } from './Card';
-// export { default as Navbar } from './Navbar';
-
