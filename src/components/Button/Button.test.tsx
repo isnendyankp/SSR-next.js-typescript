bcd5bdecf98432af31972f63823ea8c70140b3b8
isnendyankp <isnendyankp@gmail.com>
2024-01-26 21:17:08 +0700

    Add test for rendering button with label

--- src/components/Button/Button.test.tsx
+++ src/components/Button/Button.test.tsx
@@ -2,6 +2,8 @@ import Button from ".";
 import { render, screen } from "@testing-library/react";
 
 describe('Button component unit testing', () => {
+
+// this test for button component with explanation should render button with label
   it('should render button with label', () => {
     render(<Button label="Click me" />);
     const buttonElement = screen.getByText(/click me/i);
